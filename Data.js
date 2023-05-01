20de8251ae6eba75beab3ef9bc4a2157329025d9
Karan Guleria <63366256+karan0-7@users.noreply.github.com>
2023-05-01 19:34:59 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,22 @@
+const Data = [
+  {value: "Ac",classN:"reset"},
+  {value: "C",classN:"reset"},
+  {value: "/",classN:"operands"},
+  {value: "*",classN:"operands"},
+  {value: "7",classN:"numbers"},
+  {value: "8",classN:"numbers"},
+  {value: "9",classN:"numbers"},
+  {value: "-",classN:"operands"},
+  {value: "4",classN:"numbers"},
+  {value: "5",classN:"numbers"},
+  {value: "6",classN:"numbers"},
+  {value: "+",classN:"operands"},
+  {value: "1",classN:"numbers"},
+  {value: "2",classN:"numbers"},
+  {value: "3",classN:"numbers"},
+  {value: "0",classN:"numbers"},
+  {value: ".",classN:"numbers"},
+  {value: "=",classN:"operands"}
+]
+
+export default Data;
